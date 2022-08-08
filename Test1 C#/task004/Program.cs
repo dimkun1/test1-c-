0d28da39bb5b7b80a30d490af184364813c2afb3
Dima <dimku@ma5.ru>
2022-08-09 00:40:22 +0300

    add task dz  commit1

---
+++ Test1 C#/task004/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+
+Console.Write("Введите целове число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while (2, count == a);
+{
+if (count % 2 == 0) 
+{
+    Console.WriteLine("Да");
+}
+else
+{   
+    Console.WriteLine("Нет");
+}
+}
