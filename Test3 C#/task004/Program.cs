b8228b71eb29020690150d0638158d7df48992c9
Dima <dimku@ma5.ru>
2022-08-18 01:09:07 +0300

    дз4 1

--- Test3 C#/task004/Program.cs
+++ Test3 C#/task004/Program.cs
@@ -8,17 +8,17 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.Write("Ведите кординату x для первой точки: ");
-int x1 = int.Parse(Console.ReadLine());
+int x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Ведите кординату y для первой точки: ");
-int y1 = int.Parse(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Ведите кординату z для первой точки: ");
-int z1 = int.Parse(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Ведите кординату x для второй точки: ");
-int x2 = int.Parse(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Ведите кординату y для второй точки: ");
-int y2 = int.Parse(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Ведите кординату z для второй точки: ");
-int z2 = int.Parse(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
 
 double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
 // Console.WriteLine("Расстояние между точками {0: #.##}", result);
---
+++ Test4 C#/task001/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Ведите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+
+while(x > 0)
+{
+    int a = x % 10;
+    summa = summa + a;
+    x = x / 10;
+}
+Console.WriteLine(summa);
+
