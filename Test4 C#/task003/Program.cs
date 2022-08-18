2bf9164772f580df3b308a191bfd086f54884550
Dima <dimku@ma5.ru>
2022-08-18 15:56:25 +0300

    дз4 3

---
+++ Test4 C#/task003/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+// // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+
+
+
+// int[] NewArray()
+// {
+// int[] array = new int[8];
+// for(int i = 0; i <= 7; i++)
+// {
+//     array[i] = new Random().Next(0, 100);
+// }
+
+//     return array;
+// }
+
+// int[] array = NewArray();
+
+// for(int i = 0; i <= 7; i++)
+// {
+//     Console.Write($"{array[i]}, ");
+// }
+
+
+
+int[] num = new int[8];
+Random random = new Random();
+for(int i = 0; i < num.Length; i++)
+{
+    num[i] = random.Next(1, 100);
+    Console.WriteLine(num[i]);
+
+} 
