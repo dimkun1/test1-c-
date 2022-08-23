cb5ec1aa51092c805f761b48c135c485dbfa5265
Dima <dimku@ma5.ru>
2022-08-23 18:01:53 +0300

    дз5 1

---
+++ Test5 C#/task003/Program.cs
@@ -0,0 +1,47 @@
+﻿
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] GetSumPosElemNechet(int[] array)
+{
+    int sumnechet = 0;
+
+    for (int i = 1; i < array.Length; i = i + 2) sumnechet += array[i];
+
+    return new int[]{sumnechet};
+
+}
+
+int[] arr = CreateArrayRndInt(4, -100, 100);
+PrintArray(arr);
+int[] sumnechet = GetSumPosElemNechet(arr);
+Console.WriteLine($"Сумма чисел на нечетных индексах = {sumnechet[0]}");
