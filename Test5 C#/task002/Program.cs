fe9ab8db9ab3d5e08259960363750a663539dd61
Dima <dimku@ma5.ru>
2022-08-23 18:02:45 +0300

    дз5 2

---
+++ Test5 C#/task002/Program.cs
@@ -0,0 +1,43 @@
+﻿
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+    Console.Write(" -> ");
+    int count = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0) count++;
+    }
+
+    Console.Write(count);
+}
+
+
+int[] arr = CreateArrayRndInt(10, 100, 999);
+PrintArray(arr);
