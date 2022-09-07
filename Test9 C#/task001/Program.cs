2806aef4f985e506e729bf388f5a4871c53af3eb
Dima <dimku@ma5.ru>
2022-09-07 12:18:01 +0300

    дз9 -1

--- Test9 C#/task001/Program.cs
+++ Test9 C#/task001/Program.cs
@@ -1,97 +1,28 @@
-﻿
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
-// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-
-// Например, задан массив:
-
-// 1 4 7 2
-
-// 5 9 2 3
-
-// 8 4 2 4
-
-// 5 2 6 7
-
-// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 
-int[,] CreateArrayRnd(int row, int col, int min, int max)
+
+Console.Clear();
+int a = InputNumbers("Введите число ");
+NatNum(a);
+
+
+
+int InputNumbers(string input)
 {
-    int[,] matrix = new int[row, col];
-    Random rand = new Random();
-
-    for (int i = 0; i < matrix.GetLength(0); i++)       // строки row
-    {
-        for(int j = 0; j < matrix.GetLength(1); j++)        // столбцы col
-        {
-            matrix[i, j] = rand.Next(min, max);
-        }
-        
-    }
-    return matrix;
-
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
 }
 
-
-
-
-
-void PrintArray(int[,] matrix)
+void NatNum(int a)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    if (a != 0)
     {
-        
-        Console.Write("[");
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 1}, ");
-            else Console.Write($"{matrix[i, j], 1}");
-            
-        }
-        Console.WriteLine("]");
-        Console.WriteLine();
+        NatNum(a - 1);
+        Console.Write(a + " ");
     }
 }
-
-
-
-int[,] arr = CreateArrayRnd(4, 4, 0, 9);
-PrintArray(arr);
-
-
-
-
-
-
-
-
-int SumLineElements(int[,] matrix, int i)
-{
-  int sumLine = matrix[i,0];
-  for (int j = 1; j < matrix.GetLength(1); j++)
-  {
-    sumLine += matrix[i,j];
-  }
-  return sumLine;
-}
-
-
-
-
-
-int minSumLine = 0;
-int sumLine = SumLineElements(arr, 0);
-for (int i = 1; i < arr.GetLength(0); i++)
-{
-  int tempSumLine = SumLineElements(arr, i);
-  if (sumLine > tempSumLine)
-  {
-    sumLine = tempSumLine;
-    minSumLine = i;
-  }
-}
-
-Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
-
-
-
